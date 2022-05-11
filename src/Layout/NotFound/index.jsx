d3f8616c470896185f1adbccf5973c7923ea6189
Xpy <965732851@qq.com>
2022-05-11 15:35:22 +0800

    转移库

--- src/Layout/NotFound/index.jsx
+++ src/Layout/NotFound/index.jsx
@@ -2,11 +2,23 @@
  * @Author: xpy
  * @Description: 404 页面
  * @Date: 2022-04-28 14:44:44
- * @LastEditTime: 2022-04-28 14:44:44
+ * @LastEditTime: 2022-05-11 15:34:05
  */
+import { useLocation, useNavigate } from "react-router-dom";
+import { Button } from 'antd';
  
 const NotFound = () =>{
-  return <div>404</div>
+  const location = useLocation();
+  const navigator = useNavigate();
+
+  const goCss = () => {
+    navigator('/Css')
+  }
+
+  return <div>
+    
+    <Button type="primary" onClick={goCss}>跳转到首页</Button>
+  </div>
 }
 
 export default NotFound;
