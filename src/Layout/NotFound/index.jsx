5f8bbf4a633913a41c12c8b702958ccc1d653620
Xpy <965732851@qq.com>
2022-05-11 15:55:09 +0800

    修改打包路径

--- src/Layout/NotFound/index.jsx
+++ src/Layout/NotFound/index.jsx
@@ -2,13 +2,12 @@
  * @Author: xpy
  * @Description: 404 页面
  * @Date: 2022-04-28 14:44:44
- * @LastEditTime: 2022-05-11 15:34:05
+ * @LastEditTime: 2022-05-11 15:54:43
  */
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Button } from 'antd';
  
 const NotFound = () =>{
-  const location = useLocation();
   const navigator = useNavigate();
 
   const goCss = () => {
